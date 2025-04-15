0f77ab360f02443b27ff8eb431fd58403dfb3c5f
catmw <157846537+catmw@users.noreply.github.com>
2025-04-15 14:15:03 +0100

    woo

--- public/js/candidDetails.js
+++ public/js/candidDetails.js
@@ -16,15 +16,46 @@ $(document).ready(function () {
     });
     function displayCandids(candids) {
         const $table = $("#detailsTable");
+        const svg = document.getElementById("barChart");
+        const chartWidth = 1200;
+        const barHeight = 30;
+        const barSpacing = 20;
+
         candids
             .filter(candid => candid.NOVOTES > 0)
-            .forEach((candid) => {
+            .forEach((candid, i) => {
                 $table.append(`
                     <tr>
                         <td>${candid.PARTY_MNEMONIC}</td>
                         <td>${candid.SURNAME}, ${candid.FIRSTNAME}</td>
                         <td>${candid.NOVOTES}</td>
                     </tr>`);
+
+                const barWidth = (candid.NOVOTES/20);
+                const y = i * (barHeight + barSpacing) + 20;
+
+                const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+                rect.setAttribute("x", 50);
+                rect.setAttribute("y", y);
+                rect.setAttribute("width", barWidth);
+                rect.setAttribute("height", barHeight);
+                rect.setAttribute("fill", "#999");
+                svg.appendChild(rect);
+
+                const label = document.createElementNS("http://www.w3.org/2000/svg", "text");
+                label.setAttribute("y", y );
+                label.setAttribute("x", 40);
+                label.textContent = candid.SURNAME + ", " + candid.FIRSTNAME;
+                label.setAttribute("font-size", "13");
+                svg.appendChild(label);
+
+                const totalText = document.createElementNS("http://www.w3.org/2000/svg", "text");
+                totalText.setAttribute("x", barWidth);
+                totalText.setAttribute("y", y);
+                totalText.textContent = candid.NOVOTES;
+                label.setAttribute("font-size", "13");
+                svg.appendChild(totalText);
             });
+    
     }
 });
